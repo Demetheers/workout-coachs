0ac17531e4c08d0e4182d0b89679e14062d66018
Demetheers <romarbore@hotmail.fr>
2025-09-01 21:15:29 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,20 @@
-self.addEventListener('install', e=>{e.waitUntil((async()=>{const cache=await caches.open('wc-v1');await cache.addAll(['./','./index.html','./app.js','./manifest.json','./data.json']);})());});self.addEventListener('fetch', e=>{e.respondWith((async()=>{const r=await caches.match(e.request);return r||fetch(e.request);} )());});
+const cacheName = 'workout-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/data.json',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
